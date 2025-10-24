4fc39d6854552e97203d9cfa1d7c07c724f2cb9e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 12:48:42 +0200

    [angular] Account for spec change
    Accounts for the changes to the button spec.

--- angular/src/catalog/button.ts
+++ angular/src/catalog/button.ts
@@ -14,21 +14,19 @@
  limitations under the License.
  */
 
-import { Component, computed, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { v0_8 } from '@a2ui/web-lib';
 import { DynamicComponent } from './rendering/dynamic-component';
 
 @Component({
   selector: 'button[a2ui-button]',
-  template: `{{ resolvedLabel() }}`,
+  template: `<ng-content />`,
   host: {
     '(click)': 'handleClick()',
   },
 })
 export class Button extends DynamicComponent {
-  readonly label = input.required<v0_8.Primitives.StringValue | null>();
   readonly action = input.required<v0_8.Types.Action | null>();
-  protected resolvedLabel = computed(() => super.resolvePrimitive(this.label()) ?? '(empty)');
 
   protected handleClick() {
     const action = this.action();
