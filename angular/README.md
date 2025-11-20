422de6ff471e6d3ef2bbca2d1ed8c93a2d6e0db7
Yuzuru Tanahashi <ytanahashi@google.com>
2025-11-20 10:59:29 -0500

    Add Rizzcharts Angular app to the examples.

--- angular/README.md
+++ angular/README.md
@@ -6,6 +6,8 @@ These are sample implementations of A2UI in Angular.
 
 1. [nodejs](https://nodejs.org/en)
 
+NOTE: [For the rizzcharts app](../../a2a_agents/python/adk/samples/rizzcharts/), you will need GoogleMap API ([How to get the API key](https://developers.google.com/maps/documentation/javascript/get-api-key)) to display Google Map custome components. Please refer to [Rizzcharts README](./rizzcharts/README.md)
+
 ## Running
 
 1. Build the shared dependencies by running `npm run build` in the `web/lib` directory
@@ -13,7 +15,9 @@ These are sample implementations of A2UI in Angular.
 3. Run the relevant A2A server:
   * [For the restaurant app](../../a2a_agents/python/adk/samples/restaurant_finder/)
   * [For the contact app](../../a2a_agents/python/adk/samples/contact_lookup/)
+  * [For the rizzcharts app](../../a2a_agents/python/adk/samples/rizzcharts/)
 4. Run the relevant app:
   * `npm start -- restaurant`
   * `npm start -- contact`
+  * `npm start -- rizzcharts`
 5. Open http://localhost:4200/
---
+++ angular/projects/rizzcharts/src/components/toolbar/toolbar.ts
@@ -0,0 +1,56 @@
+/*
+ Copyright 2025 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import { inject, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectChange, MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { ChatService } from '@rizzcharts/services/chat_service';
+
+@Component({
+  selector: 'app-toolbar',
+  imports: [MatButtonModule, MatIconModule, MatToolbarModule, MatSelectModule, MatFormFieldModule, MatInputModule, FormsModule],
+  templateUrl: './toolbar.html',
+  styleUrl: './toolbar.scss',
+})
+export class Toolbar {
+  readonly chatService = inject(ChatService);
+
+  catalogs = [
+    {value: 'https://raw.githubusercontent.com/google/A2UI/refs/heads/main/specification/0.8/json/standard_catalog_definition.json', viewValue: 'Standard'},
+    {value: 'https://raw.githubusercontent.com/google/A2UI/refs/heads/main/a2a_agents/python/adk/samples/rizzcharts/rizzcharts_catalog_definition.json', viewValue: 'Rizzcharts Custom'},    
+  ];
+
+  selectedCatalog: string;
+
+  constructor() {
+    const catalog = this.catalogs[1].value;
+    this.chatService.componentCatalog = catalog;
+    this.selectedCatalog = catalog;
+  }
+
+  selectCatalog(event: MatSelectChange) {
+    const catalog = event.value;
+    console.log('selectCatalog ' + catalog);
+    this.chatService.componentCatalog = catalog;
+    this.selectedCatalog = catalog;
+  }  
+}
