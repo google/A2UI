8e1d42c43d7079bc42864b238a90ea0a30b7fd56
Paul Lewis <617438+paullewis@users.noreply.github.com>
2025-12-12 13:52:35 +0000

    [web] Update theming doc & add clone theme helper

--- samples/client/lit/shell/configs/contacts.ts
+++ samples/client/lit/shell/configs/contacts.ts
@@ -14,6 +14,7 @@
  limitations under the License.
  */
 
+import { cloneDefaultTheme } from "../theme/clone-default-theme.js";
 import { AppConfig } from "./types.js";
 import { v0_8 } from "@a2ui/web-lib";
 
@@ -28,74 +29,12 @@ const a = {
   "layout-al-c": true,
 };
 
-const audio = {
-  "layout-w-100": true,
-};
-
-const body = {
-  "typography-f-s": true,
-  "typography-fs-n": true,
-  "typography-w-400": true,
-  "layout-mt-0": true,
-  "layout-mb-2": true,
-  "typography-sz-bm": true,
-  "color-c-n10": true,
-};
-
-const button = {
-  "typography-f-sf": true,
-  "typography-fs-n": true,
-  "typography-w-500": true,
-  "layout-pt-3": true,
-  "layout-pb-3": true,
-  "layout-pl-5": true,
-  "layout-pr-5": true,
-  "layout-mb-1": true,
-  "border-br-16": true,
-  "border-bw-0": true,
-  "border-c-n70": true,
-  "border-bs-s": true,
-  "color-bgc-s30": true,
-  "color-c-n100": true,
-  "behavior-ho-80": true,
-};
-
 const heading = {
   "typography-f-sf": true,
   "typography-fs-n": true,
   "typography-w-500": true,
   "layout-mt-0": true,
   "layout-mb-2": true,
-  "color-c-n10": true,
-};
-
-const iframe = {
-  "behavior-sw-n": true,
-};
-
-const input = {
-  "typography-f-sf": true,
-  "typography-fs-n": true,
-  "typography-w-400": true,
-  "layout-pl-4": true,
-  "layout-pr-4": true,
-  "layout-pt-2": true,
-  "layout-pb-2": true,
-  "border-br-6": true,
-  "border-bw-1": true,
-  "color-bc-s70": true,
-  "border-bs-s": true,
-  "layout-as-n": true,
-  "color-c-n10": true,
-};
-
-const p = {
-  "typography-f-s": true,
-  "typography-fs-n": true,
-  "typography-w-400": true,
-  "layout-m-0": true,
-  "typography-sz-bm": true,
-  "layout-as-n": true,
   "color-c-n10": true,
 };
 
@@ -126,48 +65,8 @@ const listItem = {
   "layout-as-n": true,
 };
 
-const pre = {
-  "typography-f-c": true,
-  "typography-fs-n": true,
-  "typography-w-400": true,
-  "typography-sz-bm": true,
-  "typography-ws-p": true,
-  "layout-as-n": true,
-};
-
-const textarea = {
-  ...input,
-  "layout-r-none": true,
-  "layout-fs-c": true,
-};
-
-const video = {
-  "layout-el-cv": true,
-};
-
-const aLight = v0_8.Styles.merge(a, { "color-c-p30": true });
-const inputLight = v0_8.Styles.merge(input, { "color-c-n5": true });
-const textareaLight = v0_8.Styles.merge(textarea, { "color-c-n5": true });
-const buttonLight = v0_8.Styles.merge(button, { "color-c-n100": true });
-const h1Light = v0_8.Styles.merge(heading, { "color-c-n5": true });
-const h2Light = v0_8.Styles.merge(heading, { "color-c-n5": true });
-const h3Light = v0_8.Styles.merge(heading, { "color-c-n5": true });
-const h4Light = v0_8.Styles.merge(heading, { "color-c-n5": true });
-const h5Light = v0_8.Styles.merge(heading, { "color-c-n5": true });
-const bodyLight = v0_8.Styles.merge(body, { "color-c-n5": true });
-const pLight = v0_8.Styles.merge(p, { "color-c-n60": true });
-const preLight = v0_8.Styles.merge(pre, { "color-c-n35": true });
-const orderedListLight = v0_8.Styles.merge(orderedList, {
-  "color-c-n35": true,
-});
-const unorderedListLight = v0_8.Styles.merge(unorderedList, {
-  "color-c-n35": true,
-});
-const listItemLight = v0_8.Styles.merge(listItem, {
-  "color-c-n35": true,
-});
-
 const theme: v0_8.Types.Theme = {
+  ...cloneDefaultTheme(),
   additionalStyles: {
     Card: {
       "min-width": "320px",
@@ -424,37 +323,6 @@ const theme: v0_8.Types.Theme = {
       "border-br-5": true,
       "layout-el-cv": true,
     },
-  },
-  elements: {
-    a: aLight,
-    audio,
-    body: bodyLight,
-    button: buttonLight,
-    h1: h1Light,
-    h2: h2Light,
-    h3: h3Light,
-    h4: h4Light,
-    h5: h5Light,
-    iframe,
-    input: inputLight,
-    p: pLight,
-    pre: preLight,
-    textarea: textareaLight,
-    video,
-  },
-  markdown: {
-    p: [...Object.keys(pLight)],
-    h1: [...Object.keys(h1Light)],
-    h2: [...Object.keys(h2Light)],
-    h3: [...Object.keys(h3Light)],
-    h4: [...Object.keys(h4Light)],
-    h5: [...Object.keys(h5Light)],
-    ul: [...Object.keys(unorderedListLight)],
-    ol: [...Object.keys(orderedListLight)],
-    li: [...Object.keys(listItemLight)],
-    a: [...Object.keys(aLight)],
-    strong: [],
-    em: [],
   },
 };
 
