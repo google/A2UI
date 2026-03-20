affbf6ba442f7faee08fbfb2bf0c10aa89d2962f
David Iglesias Teixeira <ditman@gmail.com>
2026-03-20 16:07:52 -0700

    Make the A2uiSurface render to the shadow DOM.

--- renderers/lit/src/v0_9/surface/A2uiSurface.ts
+++ renderers/lit/src/v0_9/surface/A2uiSurface.ts
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { html, nothing, LitElement, PropertyValues } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import { SurfaceModel, ComponentContext } from "@a2ui/web_core/v0_9";
-import "./render-node.js";
+import { renderA2uiNode } from "./render-node.js";
 
 @customElement("a2ui-surface")
 export class A2uiSurface extends LitElement {
@@ -9,10 +25,6 @@ export class A2uiSurface extends LitElement {
   
   @state() accessor _hasRoot = false;
   private unsub?: () => void;
-
-  createRenderRoot() {
-    return this;
-  }
 
   protected willUpdate(changedProperties: PropertyValues) {
     if (changedProperties.has('surface')) {
@@ -51,7 +63,7 @@ export class A2uiSurface extends LitElement {
     
     try {
         const rootContext = new ComponentContext(this.surface, "root", "/");
-        return html`<a2ui-node .context=${rootContext}></a2ui-node>`;
+        return html`${renderA2uiNode(rootContext)}`;
     } catch (e) {
         console.error("Error creating root context:", e);
         return html`<div>Error rendering surface</div>`;
