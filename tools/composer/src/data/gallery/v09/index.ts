c6ad70bb7c4d35c04821a594e1dd6b811c5264e4
Lukas Moschitz <lukas@moschitz.com>
2026-03-26 17:20:52 +0100

    Add Google license headers to all new source files

--- tools/composer/src/data/gallery/v09/index.ts
+++ tools/composer/src/data/gallery/v09/index.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { V09_FLIGHT_STATUS_GALLERY } from './flight-status';
 import { V09_EMAIL_COMPOSE_GALLERY } from './email-compose';
 import { V09_CALENDAR_DAY_GALLERY } from './calendar-day';
