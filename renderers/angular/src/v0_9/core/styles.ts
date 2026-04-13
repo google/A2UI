d39e5dbb8b706cf7a0d4ead8bf3d5c37296c4841
David Iglesias Teixeira <ditman@gmail.com>
2026-04-13 16:38:56 -0700

    Add a way to provideBasicCatalogStyles from Angular

---
+++ renderers/angular/src/v0_9/core/styles.ts
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { provideEnvironmentInitializer } from '@angular/core';
+import { injectBasicCatalogStyles } from '@a2ui/web_core/v0_9/basic_catalog';
+
+/**
+ * Provides the basic catalog styles for A2UI components.
+ *
+ * This provider uses `provideEnvironmentInitializer` to ensure that the styles
+ * are injected into the document when the application starts.
+ */
+export function provideBasicCatalogStyles() {
+  return provideEnvironmentInitializer(() => {
+    injectBasicCatalogStyles();
+  });
+}
