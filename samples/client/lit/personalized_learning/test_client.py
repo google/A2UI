d5f2001a6939ef0d79d449f2857064780570a7f6
Mandar Deolalikar <11888634+dmandar@users.noreply.github.com>
2026-04-20 17:30:33 -0700

    refactor: restore deleted tests and place them in correct directories

--- samples/client/lit/personalized_learning/test_client.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from .api_server import app
-
-class TestClient(unittest.TestCase):
-    def test_app_initialization(self):
-        self.assertIsNotNone(app)
-
-if __name__ == "__main__":
-    unittest.main()
