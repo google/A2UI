1d0930d1b4d09c4fd4f52b3f90c7dc2934c5ebcc
dmandar <11888634+dmandar@users.noreply.github.com>
2026-04-20 17:22:29 -0700

    Update test_client.py

--- samples/client/lit/personalized_learning/test_client.py
+++ samples/client/lit/personalized_learning/test_client.py
@@ -2,8 +2,8 @@ import unittest
 from .api_server import app
 
 class TestClient(unittest.TestCase):
-      def test_app_initialization(self):
-                self.assertIsNotNone(app)
+    def test_app_initialization(self):
+        self.assertIsNotNone(app)
 
-  if __name__ == "__main__":
-        unittest.main()
+if __name__ == "__main__":
+    unittest.main()
