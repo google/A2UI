37287641410e974ec60e313be94e9e6af7a47060
Polina Cherkasova <polina.c@live.com>
2026-04-21 11:40:27 -0700

    -

--- samples/client/flutter/restaurant_finder/lib copy/chat_session.dart
+++ samples/client/flutter/restaurant_finder/lib copy/chat_session.dart
@@ -2,27 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// ignore_for_file: avoid_print
-
-import 'package:flutter_test/flutter_test.dart';
-
-import 'test_infra/restaurant_finder.dart';
-
-void main() {
-  setUpAll(() async {
-    final restaurantFinderClient = TestRestaurantFinderClient();
-    addTearDown(restaurantFinderClient.dispose);
-    await restaurantFinderClient.startAndVerify();
-  });
-
-  test('Restaurant finder can answer questions.', () {});
-}
-
-
-// Copyright 2025 The Flutter Authors.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
