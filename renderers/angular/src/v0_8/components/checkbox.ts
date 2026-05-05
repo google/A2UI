531bdd50eb036bfdca1ea54c01ae8e57eb61ff27
Yuzuru <ytanahashi@google.com>
2026-05-05 15:40:44 +0000

    refactor: rename checked input property to value in Checkbox component

--- renderers/angular/src/v0_8/components/checkbox.ts
+++ renderers/angular/src/v0_8/components/checkbox.ts
@@ -42,9 +42,9 @@ import type { BooleanValue, CheckboxNode, StringValue } from '../types';
 })
 export class Checkbox extends DynamicComponent<CheckboxNode> {
   readonly label = input.required<StringValue | null>();
-  readonly checked = input.required<BooleanValue | null>();
+  readonly value = input.required<BooleanValue | null>();
 
-  protected inputChecked = computed(() => super.resolvePrimitive(this.checked()) ?? false);
+  protected inputChecked = computed(() => super.resolvePrimitive(this.value()) ?? false);
   protected resolvedLabel = computed(() => super.resolvePrimitive(this.label()));
   protected readonly inputId = super.getUniqueId('a2ui-checkbox');
 
