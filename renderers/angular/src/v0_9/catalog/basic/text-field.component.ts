07c44e34b1a7d726ef0128ff81278c4e0acde600
Jose Montes <josemontesp@google.com>
2026-05-08 12:28:43 -0700

    Fix formatting in TextField component

--- renderers/angular/src/v0_9/catalog/basic/text-field.component.ts
+++ renderers/angular/src/v0_9/catalog/basic/text-field.component.ts
@@ -18,7 +18,6 @@ import {Component, computed, ChangeDetectionStrategy} from '@angular/core';
 import {BasicCatalogComponent} from './basic-catalog-component';
 import {TextFieldApi} from '@a2ui/web_core/v0_9/basic_catalog';
 
-
 /**
  * Angular implementation of the A2UI TextField component (v0.9).
  *
